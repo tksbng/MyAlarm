cc65ee001634faec93e20636b1ad86b6710a82b4
Takeshi Bingo <bingotakeshi@Takeshi-no-MacBook-Air.local>
2013-08-13 11:00:42 +0900

    Initial Commit

---
+++ MyAlarmTests/MyAlarmTests.h
@@ -0,0 +1,13 @@
+//
+//  MyAlarmTests.h
+//  MyAlarmTests
+//
+//  Created by Takeshi Bingo on 2013/08/13.
+//  Copyright (c) 2013年 Takeshi Bingo. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MyAlarmTests : SenTestCase
+
+@end
---
+++ MyAlarmTests/MyAlarmTests.m
@@ -0,0 +1,32 @@
+//
+//  MyAlarmTests.m
+//  MyAlarmTests
+//
+//  Created by Takeshi Bingo on 2013/08/13.
+//  Copyright (c) 2013年 Takeshi Bingo. All rights reserved.
+//
+
+#import "MyAlarmTests.h"
+
+@implementation MyAlarmTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MyAlarmTests");
+}
+
+@end
