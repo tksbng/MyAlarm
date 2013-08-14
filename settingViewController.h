b789a19570fa6f78ef2507fbcd594c09af580c3f
Takeshi Bingo <bingotakeshi@Takeshi-no-MacBook-Air.local>
2013-08-14 16:59:54 +0900

    test

---
+++ settingViewController.h
@@ -0,0 +1,13 @@
+//
+//  settingViewController.h
+//  MyAlarm
+//
+//  Created by Takeshi Bingo on 2013/08/13.
+//  Copyright (c) 2013年 Takeshi Bingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface settingViewController : UIViewController
+
+@end
